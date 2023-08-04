d96b6ad3ccb6f18dc3e9272127affcaf457a2227
TK <tgrl@protonmail.com>
2023-08-04 12:27:22 +0300

    Added Product ViewModel

---
+++ ShoppingListApp/ViewModels/ProductViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ShoppingListApp.ViewModels
+{
+    public class ProductViewModel
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+    }
+}
