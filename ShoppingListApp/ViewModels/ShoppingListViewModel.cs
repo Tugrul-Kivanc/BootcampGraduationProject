dc0ee9d148538b8fb82c46738217e6c0cd2d06e2
TK <tgrl@protonmail.com>
2023-08-08 17:18:10 +0300

    Added ShoppingListId

--- ShoppingListApp/ViewModels/ShoppingListViewModel.cs
+++ ShoppingListApp/ViewModels/ShoppingListViewModel.cs
@@ -2,6 +2,7 @@
 {
     public class ShoppingListViewModel
     {
+        public int ShoppingListId { get; set; }
         public int ProductId { get; set; }
         public string Name { get; set; }
         public string Image { get; set; }
