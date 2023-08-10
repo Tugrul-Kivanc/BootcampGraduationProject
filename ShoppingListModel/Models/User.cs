f8f894cbe3a8087691bf96855c64cb24037df6c8
TK <tgrl@protonmail.com>
2023-08-10 12:04:40 +0300

    Removed ConfirmPassword field

--- ShoppingListModel/Models/User.cs
+++ ShoppingListModel/Models/User.cs
@@ -13,8 +13,6 @@ public partial class User
 
     public string Password { get; set; } = null!;
 
-    public string ConfirmPassword { get; set; } = null!;
-
     public bool IsAdmin { get; set; } = false;
 
     public virtual ICollection<ShoppingList> ShoppingLists { get; set; } = new List<ShoppingList>();
